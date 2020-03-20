d09e4eff3c1f7d743f128217fe98666a2b7e3a8f
Matt Vogel <matt@portlandwebdev.com>
2020-03-19 21:31:50 -0700

    fix undefined

--- centeridentity.js
+++ centeridentity.js
@@ -64,7 +64,7 @@ exports.CenterIdentity = function(strength) {
 
     this.getLocation = async function() {
         return new Promise(function(resolve, reject){
-            if (navigator && navigator.geolocation && !(this.longitude && this.latitude)) {
+            if (typeof navigator !== 'undefined' && navigator.geolocation && !(this.longitude && this.latitude)) {
                 navigator.geolocation.getCurrentPosition(function(position) { 
                     return resolve(position); 
                 }.bind(this));
